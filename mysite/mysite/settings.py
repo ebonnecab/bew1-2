1c5556a334e4e9bef1d0c797910e7fdfc8dfd9a0
ebonnecab <edotclaire@gmail.com>
2019-11-09 07:59:12 -0800

    added models and migrated data

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    # 'pizza.apps.PizzaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
